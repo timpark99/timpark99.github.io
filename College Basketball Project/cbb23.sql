ba52feb300ffde8e7a2bb45cf6eb161887625fd4
Timothy Park <timothypark@Timothys-Air.satx.rr.com>
2024-02-22 11:34:15 -0600

    add files for parks and rec and college basketball

---
+++ College Basketball Project/cbb23.sql
@@ -0,0 +1,153 @@
+-- demonstrate subqueries to show conferences who have teams that won more than 28 games in the 23' season
+
+SELECT DISTINCT(CONF)
+FROM cbb23
+WHERE CONF IN
+        (SELECT CONF
+            FROM cbb23
+            WHERE W > 28)
+
+-- demonstrates subqueries to show average wins by conference and comparing to overall ncaa wins
+
+SELECT DISTINCT(CONF), AVG(W) AS average_wins,
+(SELECT AVG(W)
+FROM cbb23) AS average_ncaa_wins
+FROM cbb23
+GROUP BY CONF
+
+-- demonstrates subqueries to solve for the average of the max orbs in each conference
+
+SELECT AVG(max_orb) as avg_max_orb
+FROM
+(SELECT CONF, AVG(ORB) AS avg_orb, MAX(ORB) AS max_orb, MIN(ORB) AS min_orb
+FROM cbb23
+GROUP BY CONF) AS agg_table
+
+-- demonstrates the case statement used to measure a team's 3 point percentage category
+
+SELECT TEAM, CONF, _3P_O,
+CASE
+    WHEN _3P_O < 30 THEN 'Poor 3-Point shooting team'
+    WHEN _3P_O BETWEEN 30 AND 35 THEN 'Average 3-Point shooting team'
+    WHEN _3P_O > 35 THEN 'Proficient 3-Point shooting team'
+END AS three_point_bracket
+FROM cbb23
+
+-- the NCAA is awarding $10,000 for teams that have wins between 20 and 25
+-- and awarding $15,000 for teams that have wins between 26 and 30
+-- demonstrate the use of CASE statements to indicate awarded teams
+
+SELECT TEAM, CONF, W,
+CASE
+    WHEN W BETWEEN 20 AND 25 THEN 10000
+    WHEN W BETWEEN 26 AND 30 THEN 15000
+END AS Bonus
+FROM cbb23
+
+-- demonstrate the use of LEN to see the length of characters in TEAM
+
+SELECT TEAM, LEN(TEAM)
+FROM cbb23
+ORDER BY 2
+
+-- demonstrate the use of UPPER and LOWER to change capitalization
+
+SELECT TEAM, UPPER(TEAM), LOWER(TEAM)
+FROM cbb23
+ORDER BY 1
+
+-- demonstrate the use of TRIM, LTRIM, and RTRIM to get rid of white spaces
+
+SELECT TRIM('         sky        ')
+SELECT LTRIM('         sky        ')
+SELECT RTRIM('         sky        ') 
+
+-- LEFT and RIGHT will only show the number of characters you specify from the left most or right most
+-- SUBSTRING will show middle characters that you specify - a good use case would be pulling months from a date
+
+SELECT TEAM, 
+LEFT(TEAM, 4),
+RIGHT(TEAM, 4),
+SUBSTRING(TEAM,3,2)
+FROM cbb23
+
+-- REPLACE replaces a character with a character that you specify
+
+SELECT TEAM, REPLACE(TEAM, 'a', 'z')
+FROM cbb23
+
+-- use CHARINDEX to locate the position of the string
+-- you can put this into a CTE or a temp table and then filter down based on these results where it only equals 1
+
+SELECT TEAM, CHARINDEX('St', TEAM)
+FROM cbb23
+
+-- CONCAT would be useful to combine first and last name into one column
+
+SELECT TEAM, CONF,
+CONCAT(TEAM, ' ', CONF) AS team_conference
+FROM cbb23
+
+-- UNION will stack rows on top of each other distinctly
+
+SELECT TEAM, CONF
+FROM acc
+UNION
+SELECT TEAM, CONF
+FROM b10
+
+-- UNION ALL will include duplicates if there are the same TEAM and CONF in both tables.  In this situation there aren't any.
+
+SELECT TEAM, CONF
+FROM acc
+UNION ALL
+SELECT TEAM, CONF
+FROM b10
+
+-- the office must identify teams from the ACC and B10 who made the NCAA tournament
+-- using UNIONs we can accomplish that
+
+SELECT TEAM, CONF, 'NCAA Tournament Team' AS Label
+FROM acc
+WHERE SEED != 'N/A'
+UNION
+SELECT TEAM, CONF, 'NCAA Tournament Team' AS Label
+FROM b10
+WHERE SEED != 'N/A'
+ORDER BY CONF
+
+-- inner join will only bring in the rows where both tables share the same information after the ON statement
+-- shows which teams earned the same amount of wins
+
+SELECT acc.TEAM, acc.CONF, acc.W, b10.TEAM, b10.CONF, b10.W
+FROM acc
+INNER JOIN b10
+    ON acc.W = b10.W
+
+-- outer join can be left or right
+
+SELECT acc.TEAM, acc.CONF, acc.W, b10.TEAM, b10.CONF, b10.W
+FROM acc -- this is the left table
+RIGHT JOIN b10 -- this is the right table
+    ON acc.W = b10.W
+
+-- self join can modify the table to offset by 1
+
+SELECT acc.W AS acc_win,
+acc.TEAM AS acc_team,
+acc.CONF AS conference,
+b10.W AS b10_win,
+b10.TEAM AS b10_team,
+b10.CONF AS conference
+FROM acc
+JOIN b10
+    ON acc.W + 1 = b10.W
+
+-- joining multiple columns together
+
+SELECT *
+FROM acc
+INNER JOIN b10
+    ON acc.W = b10.W
+INNER JOIN cbb23
+    ON b10.EFG_O = cbb23.EFG_O
---
+++ Parks and Rec Project/parks_rec.sql
@@ -0,0 +1,163 @@
+-- demonstrate subqueries to show all data for employees who are in department_id 1
+
+SELECT *
+FROM PortfolioProject.dbo.employee_demographics
+WHERE employee_id IN
+                (SELECT employee_id
+                    FROM PortfolioProject.dbo.employee_salary
+                    WHERE dept_id = 1)
+
+-- demonstrate subqueries to show a side by side comparison of each person's salary with the average salary in the office
+
+SELECT first_name, salary,
+(SELECT AVG(salary)
+FROM employee_salary)
+FROM employee_salary
+
+-- demonstrate subqueries to find the average of a table that has already performed aggregated functions
+
+SELECT AVG(max_age)
+FROM 
+(SELECT gender, AVG(age) AS avg_age, MAX(age) AS max_age, MIN(age) AS min_age, COUNT(age) AS count_age
+FROM employee_demographics
+GROUP BY gender) AS agg_table -- table has to be named when using subqueries
+
+-- demonstrate case statement to create an age bracket
+
+SELECT first_name, last_name, age,
+CASE
+    WHEN age<= 30 THEN 'Young'
+    WHEN age BETWEEN 31 AND 50 THEN 'Old'
+    WHEN age >= 50 THEN 'On Deaths Door'
+END AS age_bracket
+FROM employee_demographics
+
+-- Pay increase and bonus
+-- if they made < 50,000 then a 5% raise
+-- if they made > 50,000 then a 7% raise
+-- if they worked in the finance department then it's a 10% bonus
+-- demonstrate case statements to satisfy these raises and bonuses
+
+SELECT first_name, last_name, salary,
+CASE
+    WHEN salary < 50000 THEN salary + (salary*0.05)
+    WHEN salary > 50000 THEN salary + (salary*0.07)
+END AS new_salary,
+CASE
+    WHEN dept_id = 6 THEN salary * 0.1
+END AS Bonus
+FROM employee_salary
+
+-- demonstrate the use of LEN to see the length of characters in first_name
+
+SELECT first_name, LEN(first_name)
+FROM employee_demographics
+ORDER BY 2
+
+-- demonstrate the use of UPPER and LOWER to change capitalization
+
+SELECT first_name, UPPER(first_name), LOWER(first_name)
+FROM employee_demographics
+ORDER BY 1
+
+-- demonstrate the use of TRIM, LTRIM, and RTRIM to get rid of white spaces
+
+SELECT TRIM('         sky        ')
+SELECT LTRIM('         sky        ')
+SELECT RTRIM('         sky        ') 
+
+-- LEFT and RIGHT will only show the number of characters you specify from the left most or right most
+-- SUBSTRING will show middle characters that you specify - a good use case would be pulling months from a date
+
+SELECT first_name, 
+LEFT(first_name, 4),
+RIGHT(first_name, 4),
+SUBSTRING(first_name,3,2),
+birth_date,
+SUBSTRING(CONVERT(char(15),birth_date,121),6,2) AS birth_month -- third argument in CONVERT is the optional style for correct date format.  CONVERT must be done because SUBSTRING only works on characters.
+FROM employee_demographics
+
+-- REPLACE replaces a character with a character that you specify
+
+SELECT first_name, REPLACE(first_name, 'a', 'z')
+FROM employee_demographics
+
+-- use CHARINDEX to locate the position of the string
+-- you can put this into a CTE or a temp table and then filter down based on these results where it only equals 1
+
+SELECT first_name, CHARINDEX('An', first_name)
+FROM employee_demographics
+
+-- CONCAT would be useful to combine first and last name into one column
+
+SELECT first_name, last_name,
+CONCAT(first_name, ' ', last_name) AS full_name
+FROM employee_demographics
+
+-- UNION will stack rows on top of each other distinctly
+
+SELECT first_name, last_name
+FROM employee_demographics
+UNION
+SELECT first_name, last_name
+FROM employee_salary
+
+-- UNION ALL will include duplicates
+
+SELECT first_name, last_name
+FROM employee_demographics
+UNION ALL
+SELECT first_name, last_name
+FROM employee_salary
+
+-- the office must identify older employees and highly paid employees to cut from the company
+-- using UNIONs we can accomplish that
+
+SELECT first_name, last_name, 'Old Man' AS Label
+FROM employee_demographics
+WHERE age > 40 AND gender = 'Male'
+UNION
+SELECT first_name, last_name, 'Old Lady' AS Label
+FROM employee_demographics
+WHERE age > 40 AND gender = 'Female'
+UNION
+SELECT first_name, last_name, 'Highly Paid Employee' AS Label
+FROM employee_salary
+WHERE salary > 70000
+ORDER BY first_name, last_name
+
+-- inner join will only bring in the rows where both tables share the same information after the ON statement
+-- ron swanson is not included in an inner join
+
+SELECT dem.employee_id, age, occupation
+FROM employee_demographics AS dem
+INNER JOIN employee_salary AS sal
+    ON dem.employee_id = sal.employee_id
+
+-- outer join can be left or right
+
+SELECT *
+FROM employee_demographics AS dem -- this is the left table
+RIGHT JOIN employee_salary AS sal -- this is the right table
+    ON dem.employee_id = sal.employee_id
+
+-- self join will give us a secret santa by moving 1 position in the employee_id
+
+SELECT emp1.employee_id AS emp_santa,
+emp1.first_name AS first_name_santa,
+emp1.last_name AS last_name_santa,
+emp2.employee_id AS emp_name,
+emp2.first_name AS first_name_emp,
+emp2.last_name AS last_name_emp
+FROM employee_salary emp1
+JOIN employee_salary emp2
+    ON emp1.employee_id + 1 = emp2.employee_id
+
+-- joining multiple columns together even though employee_demographics cannot tie with parks_departments
+
+SELECT *
+FROM employee_demographics AS dem
+INNER JOIN employee_salary AS sal
+    ON dem.employee_id = sal.employee_id
+INNER JOIN parks_departments pd
+    ON sal.dept_id = pd.department_id
