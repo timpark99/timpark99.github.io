eec46e685dec030a88e4d659e4116e284b12a8ca
Timothy Park <timpark99@yahoo.com>
2026-02-10 19:49:43 -0600

    group and aggregate data in r

--- r/filter and arrange data in r/filter and arrange data in r.R
+++ r/filter and arrange data in r/filter and arrange data in r.R
@@ -1,1 +1,20 @@
+df <- read.csv('/Users/timothypark/Documents/portfolios/timpark99.github.io/r/filter and arrange data in r/patients.csv')
+install.packages("dplyr")
+library(dplyr)
+
+df_name <- select(df, name, age)
+select(df, -satisfaction)
+select(df, patient_id:service)
+
+filter(df, age > 40)
+filter(df, service == "emergency")
+filter(df, age > 40 & service == "emergency")
+
+df %>%
+  select(patient_id:service) %>%
+  filter(age > 40 & service == "emergency") %>%
+  arrange(desc(age))
+
+arrange(df, age)
+arrange(df, desc(age))
 
---
+++ r/group and aggregate data in r/group and aggregate data in r.R
@@ -0,0 +1,2 @@
+library(dplyr)
+df <- read.csv('/Users/timothypark/Documents/portfolios/timpark99.github.io/r/group and aggregate data in r/patients.csv')
